9c3feb8db48127eebd2299690edac46ab29fc88f
choigy1001 <easytwo10@naver.com>
2022-06-03 11:58:12 +0900

    Chapter 11 예제 코드 수정 및 추가

--- src/main/java/chapter11/hashset/HashSetCapacity.java
+++ src/main/java/chapter11/hashset/HashSetCapacity.java
@@ -1,13 +1,27 @@
 package chapter11.hashset;
 
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HashSetCapacity {
     public static void main(String[] args) throws NoSuchFieldException {
         HashSet<Integer> hashSet = new HashSet<>();
+        Map<Object, Object> objectObjectMap = Collections.synchronizedMap(new HashMap<>());
+        Hashtable<Object, Object> objectObjectHashtable = new Hashtable<>();
 
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000000; i++) {
+            objectObjectMap.put(i, 10);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(start - end);
 
+        long start1 = System.currentTimeMillis();
+        for (int i = 0; i < 10000000; i++) {
+            objectObjectHashtable.put(i, 10);
+        }
+        long end1 = System.currentTimeMillis();
+        System.out.println(start1 - end1);
     }
 }
